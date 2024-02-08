78fcb2c947b249e1544742df557627ddf5fc48d2
Azeezfasasi <fasasi.azeez@yahoo.com>
2024-02-08 16:09:35 +0100

    Completed assessment page

--- js/login-pup-up.js
+++ js/login-pup-up.js
@@ -1,16 +1,3 @@
-// function togglePopup(type) {
-//   var popupLogin = document.getElementById('popup-login');
-//   var popupSignup = document.getElementById('popup-signup');
-
-//   if (type === 'login') {
-//     popupLogin.classList.toggle('active');
-//     popupSignup.classList.remove('active');
-//   } else if (type === 'signup') {
-//     popupSignup.classList.toggle('active');
-//     popupLogin.classList.remove('active');
-//   }
-// }
-
 function togglePopup(type) {
   var overlay = document.getElementById('overlay');
   var popupLogin = document.getElementById('popup-login');
