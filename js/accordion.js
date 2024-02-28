1297ea0e92d2a25070f62a90c09c413d788f3e8d
Azeezfasasi <fasasi.azeez@yahoo.com>
2024-02-28 13:49:00 +0100

    Updated

--- js/accordion.js
+++ js/accordion.js
@@ -4,12 +4,8 @@ function toggleAccordion(clickedItem) {
   accordionItems.forEach(function (item) {
     if (item !== clickedItem.parentNode) {
       item.classList.remove('active');
-      const arrow = item.querySelector('.arrow');
-      arrow.textContent = '►';
     }
   });
 
   clickedItem.parentNode.classList.toggle('active');
-  const arrow = clickedItem.querySelector('.arrow');
-  arrow.textContent = clickedItem.parentNode.classList.contains('active') ? '▼' : '►';
 }
